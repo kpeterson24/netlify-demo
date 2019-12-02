06df4d464fdaedbedfa81fc75b02c87838c2d90c
Korey Peterson <korey.t.peterson@gmail.com>
2019-12-02 09:07:29 -0600

    new kewl school stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hello Trifid</h1>
+    <p>Welcome to week 11!</p>
+    <p>Now go do something great with yourself today!</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
